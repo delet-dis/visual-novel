51c56c31541faeadfacd089fafaf94a4b94255c0
Igor Efimov <efimov.036@gmail.com>
2020-12-15 20:27:38 +0700

    added name intenting

--- app/src/main/java/com/delet_dis/vusialnovel/AcquaintanceActivity.kt
+++ app/src/main/java/com/delet_dis/vusialnovel/AcquaintanceActivity.kt
@@ -1,12 +1,26 @@
 package com.delet_dis.vusialnovel
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_acquaintance.*
+import kotlinx.android.synthetic.main.activity_acquaintance.playerName
+import kotlinx.android.synthetic.main.activity_acquaintance.setPlayerName
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class AcquaintanceActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_acquaintance)
+
+    setPlayerName.setOnClickListener {
+      if (playerName.text.toString().isNotEmpty()) {
+        val comeToWelcomeActivity = Intent(this, WelcomeActivity::class.java)
+        comeToWelcomeActivity.putExtra("playerName", playerName.text.toString())
+        startActivity(comeToWelcomeActivity)
+      }
+    }
   }
 
 }
