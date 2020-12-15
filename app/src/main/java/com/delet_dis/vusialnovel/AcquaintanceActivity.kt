72b21ddfaf7df2f5fa151009d449bfb54f098c04
Igor Efimov <efimov.036@gmail.com>
2020-12-15 18:18:05 +0700

    done adding name input activity

---
+++ app/src/main/java/com/delet_dis/vusialnovel/AcquaintanceActivity.kt
@@ -0,0 +1,11 @@
+package com.delet_dis.vusialnovel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcquaintanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acquaintance)
+    }
+}
